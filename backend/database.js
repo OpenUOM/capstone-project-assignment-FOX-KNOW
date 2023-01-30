3cfc4bd7ffaca1deda4b377ce2f35397cf09d63e
Prageeth Chamara <ygprageethchamara2002@gmail.com>
2023-01-30 15:39:11 +0530

    Implementing 7

--- backend/database.js
+++ backend/database.js
@@ -22,27 +22,13 @@ const dbinitialize = async () => {
     testBase.resetDatabase(knex_db);
 }
 
-const addTeacher = async (id, name, age) => {
-    const sql = `INSERT INTO teacher(id,name,age) values (?, ?, ?)`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql, [id, name, age])
-            .then(() => {
-                resolve({status: "Successfully inserted Teacher"})
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
 const readTeachers = async () => {
     const sql = `SELECT * FROM teacher`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
-            .then((teachers) => {
-                resolve(teachers);
+            .then((data) => {
+                resolve(data);
             })
             .catch((error) => { 
                 reject(error);
@@ -55,8 +41,22 @@ const readTeacherInfo = async (id) => {
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql, [id])
-            .then((teacher) => {
-                resolve(teacher);
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
+
+const addTeacher = async (id, name, age) => {
+    const sql = `INSERT INTO teacher(id,name,age) values (?, ?, ?)`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql, [id, name, age])
+            .then(() => {
+                resolve({status: "Successfully inserted Teacher"})
             })
             .catch((error) => {
                 reject(error);
@@ -92,27 +92,13 @@ const deleteTeacher = async (id) => {
     });
 }
 
-const addStudent = async (id, name, age, hometown) => {
-    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?, ?)`
-    return new Promise((resolve, reject) => {
-        knex_db
-            .raw(sql, [id, name, age, hometown])
-            .then(() => {
-                resolve({status: "Successfully inserted Student"})
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-}
-
 const readStudents = async () => {
     const sql = `SELECT * FROM student`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
-            .then((students) => {
-                resolve(students);
+            .then((student) => {
+                resolve(student);
             })
             .catch((error) => {
                 reject(error);
@@ -134,13 +120,25 @@ const readStudentInfo = async (id) => {
     });
 }
 
+const addStudent = async (id, name, age, hometown) => {
+    const sql = `INSERT INTO student(id,name,age,hometown) values (?, ?, ?, ?)`
+    return new Promise((resolve, reject) => {
+        knex_db
+            .raw(sql, [id, name, age, hometown])
+            .then(() => {
+                resolve({status: "Successfully inserted Student"})
+            })
+            .catch((error) => {
+                reject(error);
+            });
+    });
+}
 
-
-const updateStudent = async (name, age, hometown, id) => {
+const updateStudent = async (name, age, id, hometown) => {
     const sql = `UPDATE student SET name=?, age=?, hometown=? WHERE id=?`
     return new Promise((resolve, reject) => {
         knex_db
-            .raw(sql, [name, age, hometown, id])
+            .raw(sql, [name, age, id, hometown])
             .then(() => {
                 resolve({status: "Successfully updated Student"})
             })
@@ -165,14 +163,14 @@ const deleteStudent = async (id) => {
 }
 
 module.exports = {
-    addTeacher,
     readTeachers,
-    readTeacherInfo,
-    updateTeacher,
-    deleteTeacher,
-    addStudent,
     readStudents,
+    addStudent,
+    addTeacher,
+    deleteTeacher,
+    deleteStudent,
     readStudentInfo,
+    readTeacherInfo,
     updateStudent,
-    deleteStudent
+    updateTeacher,
 }
